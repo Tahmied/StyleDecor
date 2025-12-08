80af55de84515a7546084a3d59e70a894ec08910
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-08 13:02:19 +0600

    - configured next auth with my custom express backend for login

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,77 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" }
+      },
+      
+      async authorize(credentials) {
+        try {
+          const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/login`, {
+            method: 'POST',
+            body: JSON.stringify({
+              email: credentials.email,
+              password: credentials.password
+            }),
+            headers: { "Content-Type": "application/json" }
+          });
+
+          const response = await res.json();
+
+          if (response.statusCode === 200 && response.data) {
+            return {
+              _id: response.data.user._id,
+              email: response.data.user.email,
+              name: response.data.user.fullName || response.data.user.name,
+              image: response.data.user.image, 
+              accessToken: response.data.accessToken, 
+              refreshToken: response.data.refreshToken
+            };
+          }
+          
+          return null;
+
+        } catch (error) {
+          console.error("Login Error:", error);
+          return null;
+        }
+      }
+    })
+  ],
+  
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id;
+        token.accessToken = user.accessToken;
+        token.refreshToken = user.refreshToken;
+        token.image = user.image;
+      }
+      return token;
+    },
+    
+    async session({ session, token }) {
+      session.user._id = token._id;
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.image = token.image;
+      return session;
+    }
+  },
+  
+  pages: {
+    signIn: '/login',
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
