e86da2f371a1894847324188bc60098c93207acb
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-18 13:00:44 +0600

    - fixed useSearchParams() should be wrapped in a suspense boundary for services page

--- app/services/layout.tsx
+++ app/services/layout.tsx
@@ -1,5 +1,8 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Loader from "@/components/Utils/Loader";
+import { Suspense } from "react";
+
 export default function HomepageLayout({
     children,
 }: {
@@ -10,7 +13,9 @@ export default function HomepageLayout({
             <Header height={'auto'}>
                 <p></p>
             </Header>
-            <main>{children}</main>
+            <Suspense fallback={<Loader />}>
+                {children}
+            </Suspense>
             <Footer />
         </>
     );
