61090a35792aadfb20884b1c74a3aec219c3b8fa
Tahmied Hossain <tahmiedhossain4671@gmail.com>
2025-12-18 12:57:22 +0600

    - fixed useSearchParams() should be wrapped in a suspense boundary issue for vercel deployment

--- app/payment/layout.tsx
+++ app/payment/layout.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Loader from "@/components/Utils/Loader";
+import { Suspense } from "react";
 export default function HomepageLayout({
     children,
 }: {
@@ -10,7 +12,12 @@ export default function HomepageLayout({
             <Header height={'auto'}>
                 <p></p>
             </Header>
-            <main>{children}</main>
+            <main>
+                <Suspense fallback={<Loader />}>
+                    {children}
+                </Suspense>
+
+            </main>
             <Footer />
         </>
     );
